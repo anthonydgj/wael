c3808427d38ca0f162ef27505ac693d19fca2687
Anthony DGJ <anthonydgj4@gmail.com>
2025-04-12 14:35:15 -0400

    add initial xterm

--- wael-website/src/routes/+page.svelte
+++ wael-website/src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
-    import { base } from '$app/paths';
+	import { Xterm, XtermAddon } from '@battlefieldduck/xterm-svelte';
+	import type {
+		ITerminalOptions,
+		ITerminalInitOnlyOptions,
+		Terminal
+	} from '@battlefieldduck/xterm-svelte';
+
+	let terminal: Terminal;
+
+	let options: ITerminalOptions & ITerminalInitOnlyOptions = {
+		fontFamily: 'Consolas'
+	};
+
+	async function onLoad() {
+		console.log('Child component has loaded');
+
+		// FitAddon Usage
+		const fitAddon = new (await XtermAddon.FitAddon()).FitAddon();
+		terminal.loadAddon(fitAddon);
+		fitAddon.fit();
+
+		terminal.write('Hello World');
+	}
+
+	function onData(data: string) {
+		console.log('onData()', data);
+	}
+
+	function onKey(data: { key: string; domEvent: KeyboardEvent }) {
+		console.log('onKey()', data);
+	}
 </script>
 
-<p>
-    Home
-</p>
+<Xterm bind:terminal {options} {onLoad} {onData} {onKey} />
