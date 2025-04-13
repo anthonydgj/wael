d3aed6bb0530240e5dfa4576ada22fcfe2f295a5
Anthony DGJ <anthonydgj4@gmail.com>
2025-04-13 14:46:28 -0400

    fix build

--- wael-website/vite.config.ts
+++ wael-website/vite.config.ts
@@ -1,9 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		nodePolyfills({ 
+			include: ['fs'], 
+			overrides: { fs: 'memfs' } 
+		}),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
 });
