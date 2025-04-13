bef8e5beb2d70dabadce29252dfb9fd53b4a9afe
Anthony DGJ <anthonydgj4@gmail.com>
2025-04-13 18:20:26 -0400

    remove wael project on GitHub Pages branch

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Options, OutputFormat, Wael, DEFAULT_OPTIONS } from './main';
-export { BuiltInFunctions } from './interpreter/built-in-functions';
-export { Interpreter } from './interpreter/interpreter';
-export { Scope, ScopeBindings } from './interpreter/scope';
-export { GeometryType, GeoJSON, Unit, UNIT } from './interpreter/types';
-export { GRAMMAR } from './interpreter/grammar';
