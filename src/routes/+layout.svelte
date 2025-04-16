a9a3eb9c4b480ac41cc40abe20b07b6d070923ed
Anthony DGJ <anthonydgj4@gmail.com>
2025-04-15 22:35:32 -0400

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,10 +20,18 @@
 		}
 
 		.header {
-			padding: 0 0.5rem;
+			padding: 1rem;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+
+			.title {
+				margin: 0
+			}
 
 			.intro-text {
 				max-width: 70rem;
+				padding-left: 1rem;
 			}
 		}
 
@@ -36,12 +44,13 @@
 
 <div class="container">
 	<div class="header">
-		<h2>WAEL Interpreter</h2>
+		<h2 class="title">
+			<a href="https://github.com/anthonydgj/wael" target="_blank">WAEL</a>
+			Interpreter
+		</h2>
 		<div class="intro-text">
 			Type <pre style="display: inline;">&semi;&semi;</pre> then Enter 
-			to evaluate WAEL expressions. See the project 
-			<a href="https://github.com/anthonydgj/wael/blob/main/README.md" target="_blank">README</a> 
-			for details about the language syntax.
+			to evaluate WAEL expressions.
 		</div>
 	</div>
 
