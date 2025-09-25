851c7fc6c4e9a3519f13db99b77d6083403e56bd
Anthony DGJ <anthonydgj4@gmail.com>
2025-09-25 15:12:07 -0400

    fix standard library scope

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,7 @@
 export const STD_LIB = String.raw`
 export let StdLib = () => (
-    export Rotate = ((deg, origin) => (val => _Rotate(deg, origin, val)));
-    export Round = ((precision) => (val => _Round(precision, val)));
-    true
+    export let Rotate = ((deg, origin) => (val => _Rotate(deg, origin, val)));
+    export let Round = ((precision) => (val => _Round(precision, val)));
+    "StdLib"
 )
 `
