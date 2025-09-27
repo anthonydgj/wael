eb04ea0e1a42e36adb70029e23d8a20ed44509f5
Anthony DGJ <anthonydgj4@gmail.com>
2025-09-26 21:26:49 -0400

    enable package executable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { Options, OutputFormat, Wael, DEFAULT_OPTIONS } from './main';
-export { BuiltInFunctions } from './interpreter/built-in-functions';
 export { Interpreter } from './interpreter/interpreter';
 export { Scope, ScopeBindings } from './interpreter/scope';
 export { GeometryType, GeoJSON, Unit, UNIT } from './interpreter/types';
